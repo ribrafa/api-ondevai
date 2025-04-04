8002eda4e9453f6663331f197e02dcd3d1da9f07
ribrafa <rafaelribeiro.code@gmail.com>
2025-04-03 21:17:37 -0300

    Habilitando os CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita o CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Adapte o URL para o seu frontend (por exemplo, onde está rodando o seu frontend local)
+    methods: 'GET,PUT,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Accept', // Cabeçalhos permitidos
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
