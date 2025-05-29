83023b1f7511a31c8f7bfbde1799b3f97b5f2d4d
Bincoleto <marciobincoletojunior@gmail.com>
2025-05-28 21:39:40 -0300

    Alterado informações do databa.providers

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
+        host: '50.116.112.16',
         port: 3306,
-        username: 'root',
-        password: '',
-        database: 'ondevai',
+        username: 'vitali04_root_ondevai',
+        password: 'ondevai123456789',
+        database: 'vitali04_ondevai',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
